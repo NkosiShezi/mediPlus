f206e7c637acb38a8b737e78b44c4ac368ae479f
Nkosi <shezisthe@gmail.com>
2024-10-08 07:58:27 +0200

    refactored - code clean up

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,5 @@ import Dashboard from "./components/dashboard/dashboard.ts";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div id='dashboard'></div>`;
+
 Dashboard(document.querySelector<HTMLDivElement>("#dashboard")!);
